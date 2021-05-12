9f844437925dd1a4fd28fbc07b689a8e57412ecc
Serge Vinogradoff <1587149+zoopyserg@users.noreply.github.com>
2021-05-12 11:02:18 +0300

    Update .setup.sh

--- .setup.sh
+++ .setup.sh
@@ -3,6 +3,6 @@ git init
 git remote add origin https://github.com/zoopyserg/ultrasonic-aliases.git
 git fetch origin
 git checkout -b main --track origin/main
-echo 'source "~/.aliases.sh"' >> ~/.bashrc
+echo 'source ~/.aliases.sh' >> ~/.bashrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git config --global core.editor vim
