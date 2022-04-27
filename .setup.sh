b4dd1a15ac11fce6ba4fac48c51b3c068e8e3ae4
Serge Vinogradoff <1587149+zoopyserg@users.noreply.github.com>
2022-04-27 14:08:09 +0300

    Update .setup.sh

--- .setup.sh
+++ .setup.sh
@@ -6,3 +6,7 @@ git checkout -b main --track origin/main
 echo 'source ~/.aliases.sh' >> ~/.bashrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git config --global core.editor vim
+
+echo "source ~/.bash_functions/old_functions.sh" >> ~/.bashrc
+echo "source ~/.bash_functions/functions.sh" >> ~/.bashrc
+echo "source ~/.aliases.sh" >> ~/.bashrc
